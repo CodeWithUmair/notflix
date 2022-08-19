f673edb8a15c316f8bd05284f2f2bedb2544e2ca
Umair <codewithumair867@gmail.com>
2022-08-20 03:59:07 +0500

    working

---
+++ src/components/Search.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <>
+      <section className="searchbox-wrap">
+        <input
+          type="text"
+          placeholder="Search for movie..."
+          className="searchbox"
+        />
+      </section>
+    </>
+  );
+};
+
+export default Search;
