4ff57ae32c8093f9b0f2a3dd1383394988c84917
Umair <codewithumair867@gmail.com>
2022-08-22 04:42:16 +0500

    add

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -5,9 +5,13 @@ import Result from "./Result";
 function Results({ results, openPopup }) {
   return (
     <section className="results">
-      {results.map((result) => (
-        <Result key={result.imdbID} result={result} openPopup={openPopup} />
-      ))}
+      {results.map((result) =>
+        <Result key={result.imdbID} /> ? (
+          <Result key={result.imdbID} result={result} openPopup={openPopup} />
+        ) : (
+          console.log(`Error`)
+        )
+      )}
     </section>
   );
 }
